8bb5d459dac241a92b4be8cd2b28fb7f9597b2fe
Elena Tretiakova <vesnushkalena@gmail.com>
2025-09-02 10:34:47 +1000

    Fix ts error

--- src/containers/SignIn/hooks.ts
+++ src/containers/SignIn/hooks.ts
@@ -1,7 +1,6 @@
 import { useCallback, useEffect, useMemo, useState } from 'react';
 
 import {
-    axiosInstance as axiosFHIRInstance,
     getAuthorizeUrl,
     OAuthState,
     setAuthClientRedirectURL,
