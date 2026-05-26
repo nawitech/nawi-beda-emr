940f14aa01398d4255d85c862beed2257a1205d3
Alex Lipovka <alex.lipovka@gmail.com>
2026-05-26 23:29:45 +0400

    use EMR for default App

--- src/main.tsx
+++ src/main.tsx
@@ -12,11 +12,12 @@ import '@beda.software/emr/dist/style.css';
 
 // You can specify your own theme to ajdust color,
 // Use you https://github.com/beda-software/fhir-emr/blob/master/src/theme/ThemeProvider.tsx as example
-import { App } from '@beda.software/emr/containers';
 import { PatientDashboardProvider } from '@beda.software/emr/dist/components/Dashboard/contexts';
 import { dashboard } from '@beda.software/emr/dist/dashboard.config';
 import { dynamicActivate, getCurrentLocale } from '@beda.software/emr/services';
 import { ThemeProvider } from '@beda.software/emr/theme';
+
+import { App } from 'src/containers';
 
 export const AppWithContext = () => {
     useEffect(() => {
