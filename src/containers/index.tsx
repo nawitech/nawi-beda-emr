213453677259fb9b8acbdd7350368a2351a66aa9
Alex Lipovka <alex.lipovka@gmail.com>
2026-05-27 12:56:55 +0400

    remove obsolete file from base emr-template

--- src/containers/index.tsx
+++
@@ -1,6 +0,0 @@
-import { EMR } from '@beda.software/emr/containers';
-import { defaultMenuLayout } from '@beda.software/emr/dist/components/BaseLayout/Sidebar/SidebarTop/context';
-
-export function App() {
-    return <EMR menuLayout={defaultMenuLayout} />;
-}
