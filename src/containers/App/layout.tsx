aede6ae07b86a9eec1077e82c56df495501b18e1
Alex Lipovka <alex.lipovka@gmail.com>
2026-06-15 15:04:38 +0400

    remove scheduling and locations

--- src/containers/App/layout.tsx
+++ src/containers/App/layout.tsx
@@ -2,17 +2,14 @@ import { t } from '@lingui/macro';
 import { Patient } from 'fhir/r4b';
 
 import { MenuLayoutValue } from '@beda.software/emr/dist/components/BaseLayout/Sidebar/SidebarTop/context';
-import { EncountersIcon, OrganizationsIcon, PatientsIcon } from '@beda.software/emr/icons';
+import { OrganizationsIcon, PatientsIcon } from '@beda.software/emr/icons';
 
 import { matchCurrentUserRole, Role } from 'src/utils/role';
 
 export const menuLayout: MenuLayoutValue = () => {
     try {
         return matchCurrentUserRole({
-            [Role.Receptionist]: () => [
-                { label: t`Patients`, path: '/patients', icon: <PatientsIcon /> },
-                { label: t`Scheduling`, path: '/scheduling', icon: <EncountersIcon /> },
-            ],
+            [Role.Receptionist]: () => [{ label: t`Patients`, path: '/patients', icon: <PatientsIcon /> }],
             [Role.TriageNurse]: () => [{ label: t`Patients`, path: '/patients', icon: <PatientsIcon /> }],
             [Role.Clinician]: () => [{ label: t`Patients`, path: '/patients', icon: <PatientsIcon /> }],
             [Role.LabTechnician]: () => [{ label: t`Patients`, path: '/patients', icon: <PatientsIcon /> }],
@@ -20,9 +17,7 @@ export const menuLayout: MenuLayoutValue = () => {
             [Role.Cashier]: () => [{ label: t`Patients`, path: '/patients', icon: <PatientsIcon /> }],
             [Role.Administrator]: () => [
                 { label: t`Patients`, path: '/patients', icon: <PatientsIcon /> },
-                { label: t`Scheduling`, path: '/scheduling', icon: <EncountersIcon /> },
                 { label: t`Organizations`, path: '/organizations', icon: <OrganizationsIcon /> },
-                { label: t`Locations`, path: '/locations', icon: <OrganizationsIcon /> },
             ],
             [Role.Patient]: (patient: Patient) => [
                 { label: t`Patient`, path: `/patients/${patient!.id}`, icon: <PatientsIcon /> },
